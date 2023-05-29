f86d17781e3ef5db8ca0f974bd52a92cea7598df
cwadven <cwadven@naver.com>
2023-05-29 19:42:17 +0900

    feat: 정보 수정
    - 정보 수정

--- config/model_interfaces/common_model_interfaces.py
+++ config/model_interfaces/common_model_interfaces.py
@@ -15,8 +15,8 @@ class DateTimeActiveMixin(models.Model):
 
 
 class CreateModifyTimeMixin(models.Model):
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(verbose_name='생성 시간', auto_now_add=True)
+    updated_at = models.DateTimeField(verbose_name='수정 시간', auto_now=True)
 
     class Meta:
         abstract = True
