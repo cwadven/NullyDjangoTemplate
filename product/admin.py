523c23c4b414c26ce3096d1d297883489360ee51
cwadven <cwadven@naver.com>
2023-05-30 01:14:21 +0900

    feat: 초기 admin product 설정
    - 초기 admin product 설정

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
-# Register your models here.
+from product.models import ProductType, ProductItemInfoType, Product, ProductItem, ProductImage, ProductItemInfo
+
+
+class ProductTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(ProductType, ProductTypeAdmin)
+
+
+class ProductItemInfoTypeAdmin(admin.ModelAdmin):
+    pass
+
+
+admin.site.register(ProductItemInfoType, ProductItemInfoTypeAdmin)
+
+
+class ProductImageInline(admin.TabularInline):
+    model = ProductImage
+    extra = 0
+
+
+class ProductAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductImageInline,
+    ]
+
+
+admin.site.register(Product, ProductAdmin)
+
+
+class ProductItemInfoInline(admin.TabularInline):
+    model = ProductItemInfo
+    extra = 0
+
+
+class ProductItemAdmin(admin.ModelAdmin):
+    inlines = [
+        ProductItemInfoInline,
+    ]
+
+
+admin.site.register(ProductItem, ProductItemAdmin)
