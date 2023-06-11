9b3aca567d88e684a898018ee40bb73137889739
cwadven <cwadven@naver.com>
2023-06-11 23:14:06 +0900

    feat: create service for select product item info
    - create service for select product item info

---
+++ product/services.py
@@ -0,0 +1,78 @@
+from django.db.models import Q, Count, QuerySet
+from typing import List, Tuple
+
+from product.exception_codes import ProductDoesNotExistsException
+from product.models import Product, ProductItem, ProductItemInfo
+
+
+def get_active_product(product_id: int) -> Product:
+    try:
+        return Product.datetime_active_objects.get_actives().get(
+            id=product_id,
+            is_deleted=False,
+        )
+    except Product.DoesNotExist:
+        raise ProductDoesNotExistsException()
+
+
+def get_active_product_item_filter() -> QuerySet:
+    return ProductItem.datetime_active_objects.get_actives().filter(
+        is_deleted=False,
+    )
+
+
+def get_product_item_id_by_product_item_info(product_id: int, product_item_infos_information: List[str]) -> QuerySet:
+    """
+    product_item_infos_information 을 가지고 있는 product_item 의 id 를 가져온다.
+
+    get_product_item_id_by_product_item_info(1, ['빨강', 'S', '물방울'])
+    """
+    q = Q(product_item_infos__information__in=product_item_infos_information) if product_item_infos_information else Q()
+    q2 = Q(distinct_information=len(product_item_infos_information)) if product_item_infos_information else Q()
+    return get_active_product_item_filter().filter(
+        q,
+        product_id=product_id,
+    ).values(
+        'id',
+    ).annotate(
+        distinct_information=Count('product_item_infos__information', distinct=True)
+    ).filter(
+        q2,
+    ).values_list(
+        'id',
+        flat=True,
+    )
+
+
+def get_left_product_item_infos(product_id: int, info_type_id: int, product_item_infos_information: List[str]) -> Tuple[List[str], List[int]]:
+    """
+    info_type 을 선택하고, 지금까지 선택했던 product_item_info 들로 남은 product_item_info 들을 가져온다.
+
+    get_left_product_item_infos(1, 3, ['빨강', 'S']) --> ['빨강', 'S'] 는 지금까지 선택된 item_infos
+    (['물방울'], [1, 2])
+    """
+    product_item_ids = get_product_item_id_by_product_item_info(product_id, product_item_infos_information)
+    q = Q(product_item__id__in=product_item_ids) if product_item_infos_information else Q()
+
+    qs = ProductItemInfo.objects.filter(
+        q,
+        product_item__product_id=product_id,
+        product_item__is_deleted=False,
+        info_type_id=info_type_id,
+    ).values_list(
+        'information',
+        'product_item_id',
+    ).distinct().order_by(
+        'sequence'
+    )
+    information = []
+    product_item_ids = []
+    for x in qs:
+        if x[0] not in information:
+            information.append(x[0])
+        if x[1] not in product_item_ids:
+            product_item_ids.append(x[1])
+    return (
+        information,
+        product_item_ids,
+    )
---
+++ product/test/test_services.py
@@ -0,0 +1,340 @@
+from django.test import TestCase
+
+from product.exception_codes import ProductDoesNotExistsException
+from product.models import Product, ProductItem, ProductItemInfo, InfoType, ProductInfo
+from product.services import get_active_product, get_active_product_item_filter, \
+    get_product_item_id_by_product_item_info, get_left_product_item_infos
+
+
+class TestGetActiveProduct(TestCase):
+    def setUp(self):
+        self.product = Product.objects.create(
+            title='test',
+            description='test',
+            real_price=1000,
+            payment_price=1000,
+        )
+
+    def test_get_active_product_should_raise_when_is_deleted(self):
+        # Given: is_delete True 인 경우
+        self.product.is_deleted = True
+        self.product.is_active = True
+        self.product.save()
+
+        # When: 에러 반환
+        with self.assertRaises(ProductDoesNotExistsException) as e:
+            get_active_product(self.product.id)
+
+        # Then:
+        self.assertEqual(e.exception.default_detail, ProductDoesNotExistsException.default_detail)
+
+    def test_get_active_product_should_return_product(self):
+        # Given: is_delete False 인 경우
+        self.product.is_deleted = False
+        self.product.is_active = True
+        self.product.save()
+
+        # When:
+        product = get_active_product(self.product.id)
+
+        # Then: product 반환
+        self.assertEqual(product.id, self.product.id)
+
+
+class TestGetActiveProductItemFilter(TestCase):
+    def setUp(self):
+        self.product = Product.objects.create(
+            title='test',
+            description='test',
+            real_price=1000,
+            payment_price=1000,
+        )
+        self.product_item_deleted = ProductItem.objects.create(
+            product=self.product,
+            good_number='test1',
+            title='test1',
+            description='test1',
+            additional_payment_price=1000,
+            is_active=True,
+            is_deleted=True
+        )
+        self.product_item_not_deleted = ProductItem.objects.create(
+            product=self.product,
+            good_number='test2',
+            title='test2',
+            description='test2',
+            additional_payment_price=1000,
+            is_active=True,
+        )
+
+    def test_get_active_product_item_filter_should_raise_when_is_deleted(self):
+        # Given:
+        # When:
+        qs = get_active_product_item_filter()
+
+        # Then: 삭제되지 않은 것 조회 성공
+        self.assertEqual(qs.count(), 1)
+        self.assertEqual(qs[0].id, self.product_item_not_deleted.id)
+
+
+class TestGetProductItemIdByProductItemInfo(TestCase):
+    def setUp(self):
+        self.info_type_color = InfoType.objects.create(
+            name='색상',
+        )
+        self.info_type_size = InfoType.objects.create(
+            name='사이즈',
+        )
+        self.info_type_pattern = InfoType.objects.create(
+            name='패턴',
+        )
+
+        self.product = Product.objects.create(
+            title='test',
+            description='test',
+            real_price=1000,
+            payment_price=1000,
+            is_active=True,
+        )
+        self.product_info_color = ProductInfo.objects.create(
+            product=self.product,
+            info_type=self.info_type_color,
+            sequence=1,
+        )
+        self.product_info_size = ProductInfo.objects.create(
+            product=self.product,
+            info_type=self.info_type_size,
+            sequence=2,
+        )
+        self.product_info_pattern = ProductInfo.objects.create(
+            product=self.product,
+            info_type=self.info_type_pattern,
+            sequence=3,
+        )
+
+        self.product_item_r_s_b = ProductItem.objects.create(
+            product=self.product,
+            good_number='001',
+            title='Test Item1',
+            description='Test description1',
+            additional_payment_price=5000,
+            bought_count=10,
+            left_quantity=5,
+            is_sold_out=False,
+            is_active=True,
+        )
+        self.product_item_r_s_b_color = ProductItemInfo.objects.create(
+            product_item=self.product_item_r_s_b,
+            info_type=self.info_type_color,
+            sequence=1,
+            information='빨강',
+        )
+        self.product_item_r_s_b_size = ProductItemInfo.objects.create(
+            product_item=self.product_item_r_s_b,
+            info_type=self.info_type_size,
+            sequence=2,
+            information='S',
+        )
+        self.product_item_r_s_b_pattern = ProductItemInfo.objects.create(
+            product_item=self.product_item_r_s_b,
+            info_type=self.info_type_pattern,
+            sequence=1,
+            information='물방울',
+        )
+        self.product_item_r_m_b = ProductItem.objects.create(
+            product=self.product,
+            good_number='002',
+            title='Test Item2',
+            description='Test description2',
+            additional_payment_price=5000,
+            bought_count=10,
+            left_quantity=5,
+            is_sold_out=False,
+            is_active=True,
+        )
+        self.product_item_r_m_b_color = ProductItemInfo.objects.create(
+            product_item=self.product_item_r_m_b,
+            info_type=self.info_type_color,
+            sequence=1,
+            information='빨강',
+        )
+        self.product_item_r_m_b_size = ProductItemInfo.objects.create(
+            product_item=self.product_item_r_m_b,
+            info_type=self.info_type_size,
+            sequence=1,
+            information='M',
+        )
+        self.product_item_r_m_b_pattern = ProductItemInfo.objects.create(
+            product_item=self.product_item_r_m_b,
+            info_type=self.info_type_pattern,
+            sequence=1,
+            information='물방울',
+        )
+
+    def test_get_product_item_id_by_product_item_info(self):
+        # Given: 빨강을 가지고 있는 product_item 을 구합니다.
+        product_item_infos_information = ['빨강']
+
+        # When:
+        result = get_product_item_id_by_product_item_info(self.product.id, product_item_infos_information)
+
+        # Then: product_id 에 묶여있는 product_item 들중 빨강을 가지고 있는 product_item 을 구합니다.
+        self.assertEqual(result.count(), 2)
+        self.assertEqual(result[0], self.product_item_r_s_b.id)
+        self.assertEqual(result[1], self.product_item_r_m_b.id)
+
+        # Given: 빨강, S 를 가지고 있는 product_item 을 구합니다.
+        product_item_infos_information = ['빨강', 'S']
+
+        # When:
+        result = get_product_item_id_by_product_item_info(self.product.id, product_item_infos_information)
+
+        # Then: product_id 에 묶여있는 product_item 들중 빨강, S을 가지고 있는 product_item 을 구합니다.
+        self.assertEqual(result.count(), 1)
+        self.assertEqual(result[0], self.product_item_r_s_b.id)
+
+        # Given: 빨강, 물방울 를 가지고 있는 product_item 을 구합니다.
+        product_item_infos_information = ['빨강', '물방울']
+
+        # When:
+        result = get_product_item_id_by_product_item_info(self.product.id, product_item_infos_information)
+
+        # Then: product_id 에 묶여있는 product_item 들중 빨강, 물방울 가지고 있는 product_item 을 구합니다.
+        self.assertEqual(result.count(), 2)
+        self.assertEqual(result[0], self.product_item_r_s_b.id)
+        self.assertEqual(result[1], self.product_item_r_m_b.id)
+
+        # Given: product_item 을 구합니다.
+        product_item_infos_information = []
+
+        # When:
+        result = get_product_item_id_by_product_item_info(self.product.id, product_item_infos_information)
+
+        # Then: product_id 에 묶여있는 product_item 들을 전부 구합니다.
+        self.assertEqual(result.count(), 2)
+        self.assertEqual(result[0], self.product_item_r_s_b.id)
+        self.assertEqual(result[1], self.product_item_r_m_b.id)
+
+
+class TestGetLeftProductItemInfos(TestCase):
+    def setUp(self):
+        self.info_type_color = InfoType.objects.create(
+            name='색상',
+        )
+        self.info_type_size = InfoType.objects.create(
+            name='사이즈',
+        )
+        self.info_type_pattern = InfoType.objects.create(
+            name='패턴',
+        )
+
+        self.product = Product.objects.create(
+            title='test',
+            description='test',
+            real_price=1000,
+            payment_price=1000,
+            is_active=True,
+        )
+        self.product_info_color = ProductInfo.objects.create(
+            product=self.product,
+            info_type=self.info_type_color,
+            sequence=1,
+        )
+        self.product_info_size = ProductInfo.objects.create(
+            product=self.product,
+            info_type=self.info_type_size,
+            sequence=2,
+        )
+        self.product_info_pattern = ProductInfo.objects.create(
+            product=self.product,
+            info_type=self.info_type_pattern,
+            sequence=3,
+        )
+
+        self.product_item_r_s_b = ProductItem.objects.create(
+            product=self.product,
+            good_number='001',
+            title='Test Item1',
+            description='Test description1',
+            additional_payment_price=5000,
+            bought_count=10,
+            left_quantity=5,
+            is_sold_out=False,
+            is_active=True,
+        )
+        self.product_item_r_s_b_color = ProductItemInfo.objects.create(
+            product_item=self.product_item_r_s_b,
+            info_type=self.info_type_color,
+            sequence=1,
+            information='빨강',
+        )
+        self.product_item_r_s_b_size = ProductItemInfo.objects.create(
+            product_item=self.product_item_r_s_b,
+            info_type=self.info_type_size,
+            sequence=2,
+            information='S',
+        )
+        self.product_item_r_s_b_pattern = ProductItemInfo.objects.create(
+            product_item=self.product_item_r_s_b,
+            info_type=self.info_type_pattern,
+            sequence=1,
+            information='물방울',
+        )
+        self.product_item_r_m_b = ProductItem.objects.create(
+            product=self.product,
+            good_number='002',
+            title='Test Item2',
+            description='Test description2',
+            additional_payment_price=5000,
+            bought_count=10,
+            left_quantity=5,
+            is_sold_out=False,
+            is_active=True,
+        )
+        self.product_item_r_m_b_color = ProductItemInfo.objects.create(
+            product_item=self.product_item_r_m_b,
+            info_type=self.info_type_color,
+            sequence=1,
+            information='빨강',
+        )
+        self.product_item_r_m_b_size = ProductItemInfo.objects.create(
+            product_item=self.product_item_r_m_b,
+            info_type=self.info_type_size,
+            sequence=1,
+            information='M',
+        )
+        self.product_item_r_m_b_pattern = ProductItemInfo.objects.create(
+            product_item=self.product_item_r_m_b,
+            info_type=self.info_type_pattern,
+            sequence=1,
+            information='물방울',
+        )
+
+    def test_get_left_product_item_infos(self):
+        # Given: product_item_infos_information 아무것도 없을 경우
+        product_item_infos_information = []
+        # When:
+        result = get_left_product_item_infos(self.product.id, self.info_type_color.id, product_item_infos_information)
+        # Then:
+        self.assertEqual(result, (['빨강'], [self.product_item_r_s_b.id, self.product_item_r_m_b.id]))
+
+        # Given: 빨강을 알아내고 사이즈를 알고 싶은 경우
+        product_item_infos_information = ['빨강']
+        # When:
+        result = get_left_product_item_infos(self.product.id, self.info_type_size.id, product_item_infos_information)
+        # Then: M, S 정렬 순서를 기준으로 가져옵니다. (S 가 2고 M 이 1 입니다)
+        self.assertEqual(result, (['M', 'S'], [self.product_item_r_m_b.id, self.product_item_r_s_b.id]))
+
+        # Given:
+        product_item_infos_information = ['빨강', 'S']
+        # When:
+        result = get_left_product_item_infos(self.product.id, self.info_type_pattern.id, product_item_infos_information)
+        # Then:
+        self.assertEqual(result, (['물방울'], [self.product_item_r_s_b.id]))
+
+        # Given:
+        product_item_infos_information = ['물방울']
+        # When:
+        result = get_left_product_item_infos(self.product.id, self.info_type_size.id, product_item_infos_information)
+        # Then:
+        self.assertEqual(result, (['M', 'S'], [self.product_item_r_m_b.id, self.product_item_r_s_b.id]))
