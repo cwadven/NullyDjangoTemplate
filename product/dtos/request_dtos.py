c6be89eceabe5a215c032497baaed60fea6e8560
cwadven <cwadven@naver.com>
2023-06-17 19:10:06 +0900

    feat: 오타 수정
    - 오타 수정

--- product/dtos/request_dtos.py
+++ product/dtos/request_dtos.py
@@ -2,14 +2,14 @@ import attr
 
 
 @attr.s
-class ProductItemInfoInformationReqeustDTO(object):
+class ProductItemInfoInformationRequestDTO(object):
     product_item_infos_information = attr.ib(type=list)
     info_type_id = attr.ib(type=int)
     is_before_last_selection = attr.ib(type=bool)
     is_last_selection = attr.ib(type=bool)
 
     @classmethod
-    def by_request(cls, data) -> 'ProductItemInfoInformationReqeustDTO':
+    def by_request(cls, data) -> 'ProductItemInfoInformationRequestDTO':
         return cls(
             product_item_infos_information=data.get('product_item_infos_information'),
             info_type_id=data.get('info_type_id'),
